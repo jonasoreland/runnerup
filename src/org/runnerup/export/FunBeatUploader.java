1da32cf9a577ab5194b58db278758ae7f855b735
Jonas Oreland <jonas@mysql.com>
2012-11-28 19:46:22 +0100

    Add copyright

--- src/org/runnerup/export/FunBeatUploader.java
+++ src/org/runnerup/export/FunBeatUploader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 jonas.oreland@gmail.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.runnerup.export;
 
 import java.io.BufferedOutputStream;
@@ -10,14 +26,12 @@ import java.net.URL;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.runnerup.R;
 import org.runnerup.export.format.TCX;
 import org.runnerup.util.Constants.DB;
 
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
--- src/org/runnerup/view/Accounts.java
+++ src/org/runnerup/view/Accounts.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 jonas.oreland@gmail.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.runnerup.view;
 
 import java.util.ArrayList;
@@ -8,11 +24,9 @@ import org.runnerup.util.Constants;
 
 import android.app.ListActivity;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
