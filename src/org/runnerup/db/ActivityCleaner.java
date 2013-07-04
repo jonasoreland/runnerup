8d44fbd818b9ef0cb2499b3ef77efd7db820b549
Jonas Oreland <jonas.oreland@gmail.com>
2013-07-04 11:13:57 +0200

    cleanup + add a first version of trimActivity, which removes redundant trackpoints from an activity

--- src/org/runnerup/db/ActivityCleaner.java
+++ src/org/runnerup/db/ActivityCleaner.java
@@ -34,7 +34,7 @@ public class ActivityCleaner implements org.runnerup.util.Constants {
 		};
 
 		ArrayList<Long> laps = new ArrayList<Long>();
-		Cursor c = db.query(DB.LOCATION.LAP, cols, "activity_id == " + activityId,
+		Cursor c = db.query(DB.LAP.TABLE, cols, DB.LAP.ACTIVITY + " = " + activityId,
 				null, null, null, "_id", null);
 		if (c.moveToFirst()) {
 			do {
@@ -62,7 +62,7 @@ public class ActivityCleaner implements org.runnerup.util.Constants {
 				DB.LOCATION.TYPE,
 				"_id" };
 
-		Cursor c = db.query(DB.LOCATION.TABLE, cols, "activity_id == " + activityId + " and lap == " + lap,
+		Cursor c = db.query(DB.LOCATION.TABLE, cols, DB.LOCATION.ACTIVITY + " = " + activityId + " and " + DB.LOCATION.LAP + " = " + lap,
 				null, null, null, "_id", null);
 		if (c.moveToFirst()) {
 			Location lastLocation = null;
@@ -99,7 +99,7 @@ public class ActivityCleaner implements org.runnerup.util.Constants {
 		ContentValues tmp = new ContentValues();
 		tmp.put(DB.LAP.DISTANCE, sum_distance);
 		tmp.put(DB.LAP.TIME, (sum_time / 1000));
-		db.update(DB.LAP.TABLE, tmp, "activity_id == " + activityId + " and lap == " + lap, null);
+		db.update(DB.LAP.TABLE, tmp, DB.LAP.ACTIVITY + " = " + activityId + " and " + DB.LAP.LAP + " = " + lap, null);
 	}
 
 	/**
@@ -113,7 +113,7 @@ public class ActivityCleaner implements org.runnerup.util.Constants {
 				DB.LAP.DISTANCE,
 				DB.LAP.TIME
 		};
-		Cursor c = db.query(DB.LAP.TABLE, cols, "activity_id == " + activityId,
+		Cursor c = db.query(DB.LAP.TABLE, cols, DB.LAP.ACTIVITY + " = " + activityId,
 				null, null, null, "_id", null);
 		if (c.moveToFirst()) {
 			do {
@@ -126,11 +126,88 @@ public class ActivityCleaner implements org.runnerup.util.Constants {
 		ContentValues tmp = new ContentValues();
 		tmp.put(DB.ACTIVITY.DISTANCE, sum_distance);
 		tmp.put(DB.ACTIVITY.TIME, sum_time);
-		db.update(DB.ACTIVITY.TABLE, tmp, "_id == " + activityId, null);
+		db.update(DB.ACTIVITY.TABLE, tmp, "_id = " + activityId, null);
 	}
 
 	public static void recompute(SQLiteDatabase db, long activityId) {
 		recomputeLaps(db, activityId);
 		recomputeSummary(db, activityId);
+	}
+
+	public static void trim(SQLiteDatabase db, long activityId) {
+		final String[] cols = new String[] { DB.LAP.LAP };
+
+		ArrayList<Long> laps = new ArrayList<Long>();
+		Cursor c = db.query(DB.LOCATION.LAP, cols, DB.LAP.ACTIVITY + " = "
+				+ activityId, null, null, null, "_id", null);
+		if (c.moveToFirst()) {
+			do {
+				laps.add(c.getLong(0));
+			} while (c.moveToNext());
+		}
+		c.close();
+
+		for (long lap : laps) {
+			int res = trimLap(db, activityId, lap);
+			System.err.println("lap " + lap + " removed " + res + " locations");
+		}
+	}
+	
+	private static final float MIN_DISTANCE = 2f;
+
+	private static int trimLap(SQLiteDatabase db, long activityId, long lap) {
+		int cnt = 0;
+		final String[] cols = new String[] { 
+				DB.LOCATION.TIME,
+				DB.LOCATION.LATITUDE,
+				DB.LOCATION.LONGITUDE,
+				DB.LOCATION.TYPE,
+				"_id" };
+
+		Cursor c = db.query(DB.LOCATION.TABLE, cols, DB.LOCATION.ACTIVITY + " = " + activityId + " and " + DB.LOCATION.LAP + " = " + lap,
+				null, null, null, "_id", null);
+		if (c.moveToFirst()) {
+			Location p[] = { null, null };
+			do {
+				Location l = new Location("Dill poh");
+				l.setTime(c.getLong(0));
+				l.setLatitude(c.getDouble(1));
+				l.setLongitude(c.getDouble(2));
+				l.setProvider("" + c.getLong(3));
+
+				int type = c.getInt(3);
+				switch (type) {
+				case DB.LOCATION.TYPE_START:
+				case DB.LOCATION.TYPE_RESUME:
+					p[0] = l;
+					p[1] = null;
+					break;
+				case DB.LOCATION.TYPE_END:
+				case DB.LOCATION.TYPE_PAUSE:
+				case DB.LOCATION.TYPE_GPS:
+					if (p[0] == null) {
+						p[0] = l;
+						p[1] = null;
+						break;
+					} else if (p[1] == null) {
+						p[1] = l;
+					} else {
+						float d1 = p[0].distanceTo(p[1]);
+						float d2 = p[0].distanceTo(l);
+						if (Math.abs(d1 - d2) <= MIN_DISTANCE) {
+							// p[1] is redundant...prune it
+							p[1] = l;
+							cnt++;
+						} else {
+							p[0] = p[1];
+							p[1] = null;
+						}
+					}
+					break;
+				}
+			} while (c.moveToNext());
+		}
+		c.close();
+		return cnt;
 	}
 }
