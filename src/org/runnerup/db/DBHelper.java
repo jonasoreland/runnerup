870b4c71f638bace4a04e9e160c17bc8eea1a913
Jonas Oreland <jonas.oreland@gmail.com>
2014-01-18 17:38:25 +0100

    update db version for fb

--- src/org/runnerup/db/DBHelper.java
+++ src/org/runnerup/db/DBHelper.java
@@ -44,7 +44,7 @@ import android.os.Build;
 public class DBHelper extends SQLiteOpenHelper implements
 		org.runnerup.util.Constants {
 
-	private static final int DBVERSION = 22;
+	private static final int DBVERSION = 23;
 	private static final String DBNAME = "runnerup.db";
 
 	private static final String CREATE_TABLE_ACTIVITY = "create table "
