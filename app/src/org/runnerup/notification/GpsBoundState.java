125727503a90aea6e5209b1eed3e87a1d392dcee
Jonas Oreland <jonas.oreland@gmail.com>
2014-11-20 18:22:34 +0100

    Make notification non flickering

--- app/src/org/runnerup/notification/GpsBoundState.java
+++ app/src/org/runnerup/notification/GpsBoundState.java
@@ -1,25 +1,23 @@
 package org.runnerup.notification;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import org.runnerup.R;
 import org.runnerup.util.Constants;
 import org.runnerup.view.MainLayout;
-import org.runnerup.view.StartActivity;
 
+@TargetApi(Build.VERSION_CODES.FROYO)
 public class GpsBoundState implements NotificationState {
-    private final Context context;
+    private Notification notification;
 
     public GpsBoundState(Context context) {
-        this.context = context;
-    }
 
-    @Override
-    public Notification createNotification() {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
         Intent i = new Intent(context, MainLayout.class);
         i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
@@ -27,10 +25,16 @@ public class GpsBoundState implements NotificationState {
         PendingIntent pi = PendingIntent.getActivity(context, 0, i, 0);
 
         builder.setContentIntent(pi);
-        builder.setContentTitle("Activity ready");
-        builder.setContentText("Ready to start running!");
+        builder.setContentTitle(context.getString(R.string.activity_ready));
+        builder.setContentText(context.getString(R.string.ready_to_start_running));
         builder.setSmallIcon(R.drawable.icon);
+        builder.setOnlyAlertOnce(true);
 
-        return builder.build();
+        notification = builder.build();
+    }
+
+    @Override
+    public Notification createNotification() {
+        return notification;
     }
 }
--- app/src/org/runnerup/notification/NotificationState.java
+++ app/src/org/runnerup/notification/NotificationState.java
@@ -1,7 +1,10 @@
 package org.runnerup.notification;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
+import android.os.Build;
 
+@TargetApi(Build.VERSION_CODES.FROYO)
 public interface NotificationState {
     Notification createNotification();
 }
