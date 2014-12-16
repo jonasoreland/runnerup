addc6b8f435ce1210935a0a55b37099be5d7103a
Jonas Oreland <jonas.oreland@gmail.com>
2014-12-16 05:51:26 +0100

    make froyo version return NOT_SUPPORTED onInit

--- app/froyo/java/org/runnerup/tracker/component/TrackerWear.java
+++ app/froyo/java/org/runnerup/tracker/component/TrackerWear.java
@@ -17,6 +17,7 @@
 package org.runnerup.tracker.component;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 
 import org.runnerup.tracker.WorkoutObserver;
@@ -33,6 +34,11 @@ public class TrackerWear extends DefaultTrackerComponent implements WorkoutObser
     @Override
     public String getName() {
         return NAME;
+    }
+
+    @Override
+    public TrackerComponent.ResultCode onInit(final Callback callback, Context context) {
+        return TrackerComponent.ResultCode.RESULT_NOT_SUPPORTED;
     }
 
     @Override
