2ba23cbfc2e741577c3963e2d39badf71bf6e4ab
paradix <paradix@10g.pl>
2015-02-10 08:27:46 +0100

    export data points

--- app/src/org/runnerup/export/format/GoogleFitData.java
+++ app/src/org/runnerup/export/format/GoogleFitData.java
@@ -1,14 +1,13 @@
 package org.runnerup.export.format;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Pair;
 
-import org.runnerup.common.util.Constants;
 import org.runnerup.util.JsonWriter;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,87 +15,95 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.runnerup.common.util.Constants.*;
+
 /**
  * Created by LFAJER on 2015-02-03.
  */
 public class GoogleFitData {
 
     SQLiteDatabase mDB = null;
-    static Context context = null;
+    private static String projectId = null;
 
     private static final Map<Integer, String> activityType;
+
     static {
         Map<Integer, String> aMap = new HashMap<Integer, String>();
-        aMap.put(Constants.DB.ACTIVITY.SPORT_RUNNING, "8");
-        aMap.put(Constants.DB.ACTIVITY.SPORT_BIKING, "1");
+        aMap.put(DB.ACTIVITY.SPORT_RUNNING, "8");
+        aMap.put(DB.ACTIVITY.SPORT_BIKING, "1");
         activityType = Collections.unmodifiableMap(aMap);
     }
-
     private static class DataTypeField {
 
-        public DataTypeField(String name, String format) {
+        public DataTypeField(String name, Pair<String, String> format, String dbColumn) {
             this.nameValue = Pair.create(NAME, name);
-            this.formatValue = Pair.create(FORMAT, format);
+            this.formatSourceValue = Pair.create(FORMAT, format.first);
+            this.formatDataPointValue = format.second;
+            this.column = dbColumn;
         }
 
         static final String NAME = "name";
         static final String FORMAT = "format";
         Pair<String, String> nameValue = null;
-        Pair<String, String> formatValue = null;
-
+        Pair<String, String> formatSourceValue = null;
+        String formatDataPointValue = null;
+        String column = null;
     }
 
     private static final Map<DataSourceType, List<DataTypeField>> dataTypeFields;
+
     static {
         Map<DataSourceType, List<DataTypeField>> fieldsMap = new HashMap<DataSourceType, List<DataTypeField>>();
 
         List<DataTypeField> fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("activity", "integer"));
+        Pair<String, String> floatPoint = Pair.create("floatPoint", "fpVal");
+        Pair<String, String> integer = Pair.create("integer", "intVal");
+
+        fields.add(new DataTypeField("activity", integer, DB.ACTIVITY.SPORT));
         fieldsMap.put(DataSourceType.ACTIVITY_SEGMENT, fields);
 
         fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("bpm", "float"));
+        fields.add(new DataTypeField("bpm", floatPoint, DB.LOCATION.HR));
         fieldsMap.put(DataSourceType.ACTIVITY_HEARTRATE, fields);
 
         fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("latitude", "bpm"));
-        fields.add(new DataTypeField("longitude", "float"));
-        fields.add(new DataTypeField("accuracy", "float"));
-        fields.add(new DataTypeField("altitude", "float"));
+        fields.add(new DataTypeField("latitude", floatPoint, DB.LOCATION.LATITUDE));
+        fields.add(new DataTypeField("longitude", floatPoint, DB.LOCATION.LONGITUDE));
+        fields.add(new DataTypeField("accuracy", floatPoint, DB.LOCATION.ACCURANCY));
+        fields.add(new DataTypeField("altitude", floatPoint, DB.LOCATION.ALTITUDE));
         fieldsMap.put(DataSourceType.ACTIVITY_LOCATION, fields);
 
         fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("speed", "float"));
+        fields.add(new DataTypeField("speed", floatPoint, DB.LOCATION.SPEED));
         fieldsMap.put(DataSourceType.ACTIVITY_SPEED, fields);
 
         fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("activity", "integer"));
-        fields.add(new DataTypeField("duration", "integer"));
-        fields.add(new DataTypeField("num_segments", "integer"));
+        fields.add(new DataTypeField("activity", integer, DB.ACTIVITY.SPORT));
+        fields.add(new DataTypeField("duration", integer, DB.ACTIVITY.TIME));
+        fields.add(new DataTypeField("num_segments", integer, "1"));
         fieldsMap.put(DataSourceType.ACTIVITY_SUMMARY, fields);
 
         fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("average", "float"));
-        fields.add(new DataTypeField("max", "float"));
-        fields.add(new DataTypeField("min", "float"));
+        fields.add(new DataTypeField("average", floatPoint, "AVG(" + DB.LOCATION.HR + ")"));
+        fields.add(new DataTypeField("max", floatPoint, "MAX(" + DB.LOCATION.HR + ")"));
+        fields.add(new DataTypeField("min", floatPoint, "MIN(" + DB.LOCATION.HR + ")"));
         fieldsMap.put(DataSourceType.HEARTRATE_SUMMARY, fields);
 
         fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("low_latitude", "float"));
-        fields.add(new DataTypeField("high_latitude", "float"));
-        fields.add(new DataTypeField("low_longitude", "float"));
-        fields.add(new DataTypeField("high_longitude", "float"));
+        fields.add(new DataTypeField("low_latitude", floatPoint, "MIN(" + DB.LOCATION.LATITUDE + ")"));
+        fields.add(new DataTypeField("high_latitude", floatPoint, "MAX(" + DB.LOCATION.LATITUDE + ")"));
+        fields.add(new DataTypeField("low_longitude", floatPoint, "MIN(" + DB.LOCATION.LONGITUDE + ")"));
+        fields.add(new DataTypeField("high_longitude", floatPoint, "MAX(" + DB.LOCATION.LONGITUDE + ")"));
         fieldsMap.put(DataSourceType.LOCATION_SUMMARY, fields);
 
         fields = new ArrayList<DataTypeField>();
-        fields.add(new DataTypeField("average", "float"));
-        fields.add(new DataTypeField("max", "float"));
-        fields.add(new DataTypeField("min", "float"));
+        fields.add(new DataTypeField("average", floatPoint, "AVG(" + DB.LOCATION.SPEED + ")"));
+        fields.add(new DataTypeField("max", floatPoint, "MAX(" + DB.LOCATION.SPEED + ")"));
+        fields.add(new DataTypeField("min", floatPoint, "MIN(" + DB.LOCATION.SPEED + ")"));
         fieldsMap.put(DataSourceType.SPEED_SUMMARY, fields);
 
         dataTypeFields = Collections.unmodifiableMap(fieldsMap);
     }
-
     public enum DataSourceType {
 
         ACTIVITY_SEGMENT("com.google.activity.segment", "runnerup_activity_segment"),
@@ -109,6 +116,7 @@ public class GoogleFitData {
         SPEED_SUMMARY("com.google.speed.summary", "runnerup_speed_summary");
 
         final String dataType;
+
         final String dataName;
         final String sourceType = "derived";
 
@@ -118,17 +126,15 @@ public class GoogleFitData {
         }
 
         public String getDataStreamId() {
-            String packageName =  context.getPackageName();
             StringBuilder streamId = new StringBuilder();
-            streamId.append(sourceType).append(":").append(dataType).append(":").append(packageName).append(":").append(dataName);
+            streamId.append(sourceType).append(":").append(dataType).append(":").append(projectId).append(":").append(dataName);
             return streamId.toString();
         }
 
     }
-
-    public GoogleFitData(final SQLiteDatabase db, Context ctx) {
+    public GoogleFitData(final SQLiteDatabase db, String projectId) {
         this.mDB = db;
-        this.context = ctx;
+        this.projectId = projectId;
     }
 
     public void exportDataSource(DataSourceType type, Writer writer) {
@@ -159,52 +165,229 @@ public class GoogleFitData {
         for(DataTypeField field : dataTypeFields.get(type)) {
             w.beginObject();
             w.name(field.nameValue.first).value(field.nameValue.second);
-            w.name(field.formatValue.first).value(field.formatValue.second);
+            w.name(field.formatSourceValue.first).value(field.formatSourceValue.second);
             w.endObject();
         }
     }
 
     private void addApplicationObject(JsonWriter w) throws IOException {
         w.beginObject();
-        w.name("packageName");
-        w.value(context.getPackageName());
+        w.name("name").value("RunnerUp");
         w.endObject();
     }
 
     public List<DataSourceType> getActivityDataSourceTypes(long activityId) {
         List<DataSourceType> neededSources = new ArrayList<DataSourceType>();
 
-        String[] pColumns = {
-                Constants.DB.LOCATION.TIME, Constants.DB.LOCATION.HR, Constants.DB.LOCATION.SPEED
-        };
-        Cursor cursor = mDB.query(Constants.DB.LOCATION.TABLE, pColumns,
-                Constants.DB.LOCATION.ACTIVITY + " = " + activityId + " AND " + pColumns[1] + " IS NOT NULL", null, null, null,
-                null);
+        // First we export the location
+        neededSources.add(DataSourceType.ACTIVITY_LOCATION);
+        neededSources.add(DataSourceType.LOCATION_SUMMARY);
 
+        // Than if present the heart rate
+        String[] pColumns = {
+                DB.LOCATION.TIME, DB.LOCATION.HR, DB.LOCATION.SPEED
+        };
+        Cursor cursor = mDB.query(DB.LOCATION.TABLE, pColumns,
+                DB.LOCATION.ACTIVITY + " = " + activityId + " AND " + pColumns[1] + " IS NOT NULL", null, null, null,
+                null);
         if (cursor.getCount() > 0) {
             neededSources.add(DataSourceType.ACTIVITY_HEARTRATE);
             neededSources.add(DataSourceType.HEARTRATE_SUMMARY);
         }
 
-        cursor = mDB.query(Constants.DB.LOCATION.TABLE, pColumns,
-                Constants.DB.LOCATION.ACTIVITY + " = " + activityId + " AND " + pColumns[2] + " IS NOT NULL", null, null, null,
+        // Next will be the speed
+        cursor = mDB.query(DB.LOCATION.TABLE, pColumns,
+                DB.LOCATION.ACTIVITY + " = " + activityId + " AND " + pColumns[2] + " IS NOT NULL", null, null, null,
                 null);
-
         if (cursor.getCount() > 0) {
             neededSources.add(DataSourceType.ACTIVITY_SPEED);
             neededSources.add(DataSourceType.SPEED_SUMMARY);
         }
 
+        // At last the segments and summary
         neededSources.add(DataSourceType.ACTIVITY_SEGMENT);
         neededSources.add(DataSourceType.ACTIVITY_SUMMARY);
-        neededSources.add(DataSourceType.ACTIVITY_LOCATION);
-        neededSources.add(DataSourceType.LOCATION_SUMMARY);
         return neededSources;
     }
 
-    public void exportDataPoints(long activityId, String dataType) {
-
+    public String exportTypeData(DataSourceType source, long activityId, StringWriter w) {
+        String requestUrl = "";
+        switch(source) {
+            case ACTIVITY_SEGMENT:
+                requestUrl = exportActivitySegments(source, activityId, w);
+                return requestUrl;
+            case ACTIVITY_SUMMARY:
+                requestUrl = exportActivitySummary(source, activityId, w);
+                return requestUrl;
+            case ACTIVITY_LOCATION:
+            case ACTIVITY_HEARTRATE:
+            case ACTIVITY_SPEED:
+            case LOCATION_SUMMARY:
+            case HEARTRATE_SUMMARY:
+            case SPEED_SUMMARY:
+                requestUrl = exportSourceDataPoints(source, activityId, w);
+                return requestUrl;
+        }
+        return requestUrl;
     }
+
+    private String exportActivitySegments(DataSourceType source, long activityId, StringWriter writer) {
+
+        String[] pColumns = {
+                DB.ACTIVITY.START_TIME, DB.ACTIVITY.TIME, DB.ACTIVITY.SPORT
+        };
+        Cursor cursor =  mDB.query(DB.ACTIVITY.TABLE, pColumns,"_id = " + activityId, null, null, null, null);
+        cursor.moveToFirst();
+
+        //time as nanos
+        long startTime = cursor.getLong(0)*1000;
+        long endTime = (cursor.getLong(0)+cursor.getLong(1))*1000;
+
+        JsonWriter w = new JsonWriter(writer);
+        try {
+            w.beginObject();
+            w.name("minStartTimeNs").value(startTime);
+            w.name("maxEndTimeNs").value(endTime);
+            w.name("dataStreamId").value(source.getDataStreamId());
+            w.name("point");
+            w.beginArray();
+            //export points
+            w.beginObject();
+            w.name("startTimeNanos").value(startTime);
+            w.name("endTimeNanos").value(endTime);
+            w.name("dataTypeName").value(source.dataType);
+            w.name("value");
+            w.beginArray();
+            w.beginObject();
+            w.name("intVal").value(activityType.get(cursor.getInt(2)));
+            w.endObject();
+            w.endArray();
+            w.endObject();
+            //end export points
+            w.endArray();
+            w.endObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return getURLSuffix(source, startTime, endTime);
+    }
+
+    private String getURLSuffix(DataSourceType source, long startTime, long endTime) {
+        StringBuilder urlSuffix = new StringBuilder();
+        urlSuffix.append("/").append(source.getDataStreamId()).append("/datasets/").append(startTime).append("-").append(endTime);
+        return urlSuffix.toString();
+    }
+
+    private String exportSourceDataPoints(DataSourceType source, long activityId, StringWriter writer) {
+
+        ArrayList<String> pColumns = new ArrayList<String>();
+        pColumns.add("MIN("+DB.LOCATION.TIME+")");
+        pColumns.add("MAX("+DB.LOCATION.TIME+")");
+        Cursor minMaxTime =  mDB.query(DB.LOCATION.TABLE, pColumns.toArray(new String[pColumns.size()]), DB.LOCATION.ACTIVITY + " = " + activityId, null, null, null, null);
+        minMaxTime.moveToFirst();
+
+        pColumns = new ArrayList<String>();
+        pColumns.add(DB.LOCATION.TIME);
+        List<DataTypeField> fields = dataTypeFields.get(source);
+        for (DataTypeField field : fields) {
+            pColumns.add(field.column);
+        }
+
+        Cursor cursor =  mDB.query(DB.LOCATION.TABLE, pColumns.toArray(new String[pColumns.size()]) , DB.LOCATION.ACTIVITY + " = " + activityId, null, null, null, null);
+        cursor.moveToFirst();
+
+        long startTime = minMaxTime.getLong(0);
+        long endTime = minMaxTime.getLong(1);
+
+        JsonWriter w = new JsonWriter(writer);
+        try {
+            w.beginObject();
+            w.name("minStartTimeNs").value(startTime);
+            w.name("maxEndTimeNs").value(endTime);
+            w.name("dataStreamId").value(source.getDataStreamId());
+            w.name("point");
+            w.beginArray();
+
+            //export points
+            do {
+                w.beginObject();
+                w.name("startTimeNanos").value(cursor.getLong(0));
+                if (!cursor.isLast()) {
+                    cursor.moveToNext();
+                    w.name("endTimeNanos").value(cursor.getLong(0));
+                    cursor.moveToPrevious();
+                } else {
+                    w.name("endTimeNanos").value(endTime);
+                }
+                w.name("dataTypeName").value(source.dataType);
+                w.name("value");
+                w.beginArray();
+                for (DataTypeField field : fields) {
+                    w.beginObject();
+                    w.name(field.formatDataPointValue).value(cursor.getString(cursor.getColumnIndex(field.column)));
+                    w.endObject();
+                }
+                w.endArray();
+                w.endObject();
+            } while (cursor.moveToNext());
+            //end export points
+            w.endArray();
+            w.endObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return getURLSuffix(source, startTime, endTime);
+    }
+
+    private String exportActivitySummary(DataSourceType source, long activityId, StringWriter writer) {
+
+        ArrayList<String> pColumns = new ArrayList<String>();
+        pColumns.add(DB.ACTIVITY.START_TIME);
+        List<DataTypeField> fields = dataTypeFields.get(source);
+        for (DataTypeField field : fields) {
+            pColumns.add(field.column);
+        }
+
+        Cursor cursor =  mDB.query(DB.ACTIVITY.TABLE, pColumns.toArray(new String[pColumns.size()]),"_id = " + activityId, null, null, null, null);
+        cursor.moveToFirst();
+
+        //time as nanos
+        long startTime = cursor.getLong(0)*1000;
+        long endTime = (cursor.getLong(0)+cursor.getLong(1))*1000;
+
+        JsonWriter w = new JsonWriter(writer);
+        try {
+            w.beginObject();
+            w.name("minStartTimeNs").value(startTime);
+            w.name("maxEndTimeNs").value(endTime);
+            w.name("dataStreamId").value(source.getDataStreamId());
+            w.name("point");
+            w.beginArray();
+            //export points
+            w.beginObject();
+            w.name("startTimeNanos").value(startTime);
+            w.name("endTimeNanos").value(endTime);
+            w.name("dataTypeName").value(source.dataType);
+            w.name("value");
+            w.beginArray();
+            for (DataTypeField field : fields) {
+                w.beginObject();
+                w.name(field.formatDataPointValue).value(cursor.getString(cursor.getColumnIndex(field.column)));
+                w.endObject();
+            }
+            w.endArray();
+            w.endObject();
+            //end export points
+            w.endArray();
+            w.endObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return getURLSuffix(source, startTime, endTime);
+    }
+
 
     public void exportSession() {
 
