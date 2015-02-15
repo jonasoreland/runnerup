8db0dd2fff67685d4e51a21eb7f8f440b0a55cc2
paradix <paradix@10g.pl>
2015-02-15 18:45:32 +0100

    gzip request support

--- app/src/org/runnerup/export/GoogleFitUploader.java
+++ app/src/org/runnerup/export/GoogleFitUploader.java
@@ -28,10 +28,10 @@ import java.util.zip.GZIPOutputStream;
 public class GoogleFitUploader extends GooglePlus implements Uploader {
 
     public static final String NAME = "GoogleFit";
-    private final Context context;
-
     public static final String REST_URL = "https://www.googleapis.com/fitness/v1/users/me/";
-
+    public static final String REST_DATASOURCE = "dataSources";
+    public static final String REST_DATASETS = "datasets";
+    public static final String REST_SESSIONS = "sessions";
     private static final String SCOPES =
             " https://www.googleapis.com/auth/fitness.activity.write " +
                     "https://www.googleapis.com/auth/fitness.activity.read " +
@@ -39,6 +39,8 @@ public class GoogleFitUploader extends GooglePlus implements Uploader {
                     "https://www.googleapis.com/auth/fitness.body.read " +
                     "https://www.googleapis.com/auth/fitness.location.write " +
                     "https://www.googleapis.com/auth/fitness.location.read";
+
+    private final Context context;
 
     private enum RequestMethod { GET, POST, PATCH, PUT; }
 
@@ -55,6 +57,10 @@ public class GoogleFitUploader extends GooglePlus implements Uploader {
     @Override
     public String getName() {
         return NAME;
+    }
+
+    public Context getContext() {
+        return context;
     }
 
     @Override
@@ -81,7 +87,7 @@ public class GoogleFitUploader extends GooglePlus implements Uploader {
         }
 
         //export DataSource if not yet existing
-        GoogleFitData gfd = new GoogleFitData(db, getProjectId());
+        GoogleFitData gfd = new GoogleFitData(db, getProjectId(), getContext());
         List<String> presentDataSources = listExistingDataSources();
         List<GoogleFitData.DataSourceType> activitySources = gfd.getActivityDataSourceTypes(mID);
 
@@ -109,18 +115,24 @@ public class GoogleFitUploader extends GooglePlus implements Uploader {
         try {
             for (GoogleFitData.DataSourceType type : activitySources) {
                 if (!presentDataSources.contains(type.getDataStreamId())) {
-                    HttpURLConnection connect = getHttpURLConnection("dataSources", RequestMethod.POST);
 
-                    BufferedWriter w = new BufferedWriter(new OutputStreamWriter(connect.getOutputStream(), "UTF-8"));
+                    StringWriter w = new StringWriter();
                     gfd.exportDataSource(type, w);
-                    w.flush();
 
-                    if (connect.getResponseCode() >= 300) {
+                    HttpURLConnection connect = getHttpURLConnection("dataSources", RequestMethod.POST);
+                    GZIPOutputStream gos = new GZIPOutputStream(connect.getOutputStream());
+                    gos.write(w.toString().getBytes());
+                    gos.flush();
+                    gos.close();
+
+                    int code = connect.getResponseCode();
+                    if (code != 200) {
                         System.out.println(parse(new GZIPInputStream(connect.getErrorStream())));
                         return Status.ERROR;
                     } else {
                         System.out.println(parse(new GZIPInputStream(connect.getInputStream())));
                     }
+
                     connect.disconnect();
                 }
             }
@@ -140,13 +152,10 @@ public class GoogleFitUploader extends GooglePlus implements Uploader {
             String suffix = gfd.exportTypeData(source, activityId, w);
 
             HttpURLConnection connect = getHttpURLConnection(suffix, RequestMethod.PATCH);
-
-
-            OutputStreamWriter ow = new OutputStreamWriter(connect.getOutputStream(), "UTF-8");
-
-            ow.write(w.toString());
-            ow.flush();
-            ow.close();
+            GZIPOutputStream gos = new GZIPOutputStream(connect.getOutputStream());
+            gos.write(w.toString().getBytes());
+            gos.flush();
+            gos.close();
 
             int code = connect.getResponseCode();
             if (code != 200) {
@@ -174,18 +183,24 @@ public class GoogleFitUploader extends GooglePlus implements Uploader {
 
             HttpURLConnection connect = getHttpURLConnection(suffix, RequestMethod.PUT);
 
-            OutputStream out = new BufferedOutputStream(connect.getOutputStream());
-            out.write(w.getBuffer().toString().getBytes());
-            out.flush();
-            out.close();
+            GZIPOutputStream gos = new GZIPOutputStream(connect.getOutputStream());
+            gos.write(w.toString().getBytes());
+            gos.flush();
+            gos.close();
 
             int code = connect.getResponseCode();
-            if (code == 200) {
+            if (code != 200) {
+                System.out.println(parse(new GZIPInputStream(connect.getErrorStream())));
+                return status;
+            } else {
+                System.out.println(parse(new GZIPInputStream(connect.getInputStream())));
                 status = Status.OK;
             }
 
             connect.disconnect();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         return status;
@@ -197,7 +212,8 @@ public class GoogleFitUploader extends GooglePlus implements Uploader {
         connect.setDoOutput(true);
         connect.addRequestProperty("Authorization", "Bearer " + getAccessToken());
         connect.addRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        connect.addRequestProperty("Accept-Encoding", "gzip, deflate");
+        connect.addRequestProperty("Content-Encoding", "gzip");
+        connect.addRequestProperty("Accept-Encoding", "gzip");
         connect.addRequestProperty("User-Agent", "RunnerUp (gzip)");
         if (requestMethod.equals(RequestMethod.PATCH)) {
             connect.addRequestProperty("X-HTTP-Method-Override", "PATCH");
