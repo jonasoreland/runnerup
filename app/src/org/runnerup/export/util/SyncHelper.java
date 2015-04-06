b7e29efe5bb9c3307069219f20dbef015a34b15a
paradix <paradix@10g.pl>
2015-04-06 14:05:00 +0200

    GPL Headers

--- app/src/org/runnerup/export/util/SyncHelper.java
+++ app/src/org/runnerup/export/util/SyncHelper.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014 paradix@10g.pl
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.runnerup.export.util;
 
 import android.content.ContentValues;
@@ -24,9 +41,6 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by LFAJER on 2015-04-04.
- */
 public final class SyncHelper {
 
     /**
