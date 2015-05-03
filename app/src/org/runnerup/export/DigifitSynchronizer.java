2c505607dd52dabd9aaa4d2d66c7f5402a726f95
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2015-05-03 10:00:54 +0200

    Fix broken "Clean uploads from phone" feature

--- app/src/org/runnerup/export/DigifitSynchronizer.java
+++ app/src/org/runnerup/export/DigifitSynchronizer.java
@@ -2,11 +2,11 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
+ * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- *    
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -27,6 +27,7 @@ package org.runnerup.export;
 
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.util.Pair;
 
 import org.apache.http.HttpStatus;
