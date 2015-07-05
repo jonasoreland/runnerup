jonasoreland/runnerup

commit 62ba68040f923860d1a0e8eaa3a9f619620fa56e

db: handle runnerup.db.export in intent-filter so that if one download it from internet and click on it it is automatically imported in runner up

app/src/org/runnerup/view/SettingsActivity.java
@@ -76,16 +76,11 @@ public class SettingsActivity extends PreferenceActivity {
         return false;
     }
 
-    private String getDbFile() {
-        String from = getFilesDir().getPath() + "/../databases/runnerup.db";
-        return from;
-    }
-
     final OnPreferenceClickListener onExportClick = new OnPreferenceClickListener() {
 
         @Override
         public boolean onPreferenceClick(Preference preference) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
             String dstdir = Environment.getExternalStorageDirectory().getPath();
             builder.setTitle("Export runnerup.db to " + dstdir);
             DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
@@ -95,7 +90,7 @@ public class SettingsActivity extends PreferenceActivity {
                 }
 
             };
-            String from = getDbFile();
+            String from = DBHelper.getDbPath(getApplicationContext());
             String to = dstdir + "/runnerup.db.export";
             try {
                 int cnt = FileUtil.copyFile(to, from);
@@ -114,27 +109,9 @@ public class SettingsActivity extends PreferenceActivity {
 
         @Override
         public boolean onPreferenceClick(Preference preference) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
             String srcdir = Environment.getExternalStorageDirectory().getPath();
-            builder.setTitle("Import runnerup.db from " + srcdir);
-            DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-
-            };
-            String to = getDbFile();
             String from = srcdir + "/runnerup.db.export";
-            try {
-                int cnt = FileUtil.copyFile(to, from);
-                builder.setMessage("Copied " + cnt + " bytes");
-                builder.setPositiveButton(getString(R.string.Great), listener);
-            } catch (IOException e) {
-                builder.setMessage("Exception: " + e.toString());
-                builder.setNegativeButton(getString(R.string.Darn), listener);
-            }
-            builder.show();
+            DBHelper.importDatabase(SettingsActivity.this, from);
             return false;
         }
     };

app/AndroidManifest.xml
@@ -57,6 +57,14 @@
 				<action android:name="android.intent.action.MAIN" />
 				<category android:name="android.intent.category.LAUNCHER" />
 			</intent-filter>
+
+			<intent-filter>
+				<action android:name="android.intent.action.VIEW" />
+				<category android:name="android.intent.category.DEFAULT" />
+				<data android:mimeType="application/octet-stream" />
+				<data android:host="*" />
+				<data android:pathPattern="runnerup.db.export" />
+			</intent-filter>
   		</activity>
 
 		<activity android:name=".view.StartActivity"

app/src/org/runnerup/db/DBHelper.java
@@ -18,9 +18,11 @@
 package org.runnerup.db;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -46,7 +48,9 @@ import org.runnerup.export.RunnerUpLiveSynchronizer;
 import org.runnerup.export.RunningAHEADSynchronizer;
 import org.runnerup.export.RuntasticSynchronizer;
 import org.runnerup.export.StravaSynchronizer;
+import org.runnerup.util.FileUtil;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -576,5 +580,31 @@ public class DBHelper extends SQLiteOpenHelper implements
             }
         }
         return result;
+    }
+
+    public static String getDbPath(Context ctx) {
+        return ctx.getFilesDir().getPath() + "/../databases/runnerup.db";
+    }
+
+    public static void importDatabase(Context ctx, String from) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+        builder.setTitle("Import runnerup.db from " + from);
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+
+        };
+        String to = getDbPath(ctx);
+        try {
+            int cnt = FileUtil.copyFile(to, from);
+            builder.setMessage("Copied " + cnt + " bytes");
+            builder.setPositiveButton(ctx.getString(R.string.Great), listener);
+        } catch (IOException e) {
+            builder.setMessage("Exception: " + e.toString());
+            builder.setNegativeButton(ctx.getString(R.string.Darn), listener);
+        }
+        builder.show();
     }
 }

app/src/org/runnerup/view/MainLayout.java
@@ -29,6 +29,7 @@ import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
@@ -140,6 +141,22 @@ public class MainLayout extends TabActivity {
 
         handleBundled(getApplicationContext().getAssets(), "bundled", getFilesDir().getPath()
                 + "/..");
+
+        // if we were called from an intent-filter because user opened "runnerup.db.export", load it
+        final Uri data = getIntent().getData();
+        if (data != null) {
+            String filePath = null;
+            if ("content".equals(data.getScheme())) {
+                Cursor cursor = this.getContentResolver().query(data, new String[] { android.provider.MediaStore.Images.ImageColumns.DATA }, null, null, null);
+                cursor.moveToFirst();
+                filePath = cursor.getString(0);
+                cursor.close();
+            } else {
+                filePath = data.getPath();
+            }
+            Log.i(getClass().getSimpleName(), "Importing database from " + filePath);
+            DBHelper.importDatabase(MainLayout.this, filePath);
+        }
     }
 
     void handleBundled(AssetManager mgr, String src, String dst) {

commit 3da377dd71b26394145450e9451764e2bb8e1eb6

db: fix crash regression from 62ba680 when exporting db

app/src/org/runnerup/view/SettingsActivity.java
@@ -88,7 +88,7 @@ public class SettingsActivity extends PreferenceActivity {
 
         @Override
         public boolean onPreferenceClick(Preference preference) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
             String dstdir = Environment.getExternalStorageDirectory().getPath();
             builder.setTitle("Export runnerup.db to " + dstdir);
             DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {