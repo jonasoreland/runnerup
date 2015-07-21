43c62ece9b5b240b1528ba5b360493cea6c91b95
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2015-07-21 21:20:50 +0200

    notification: use alpha icon for notifications to avoid full white square on android 5.0

--- app/src/org/runnerup/notification/GpsSearchingState.java
+++ app/src/org/runnerup/notification/GpsSearchingState.java
@@ -31,7 +31,7 @@ public class GpsSearchingState implements NotificationState {
 
         builder.setContentIntent(pi);
         builder.setContentTitle(context.getString(R.string.Searching_for_GPS));
-        builder.setSmallIcon(R.drawable.icon);
+        builder.setSmallIcon(R.drawable.icon_notification);
         builder.setOnlyAlertOnce(true);
         org.runnerup.util.NotificationCompat.setLocalOnly(builder);
         org.runnerup.util.NotificationCompat.setVisibility(builder);
