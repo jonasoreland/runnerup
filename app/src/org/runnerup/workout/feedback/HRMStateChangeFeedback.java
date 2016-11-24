6e4751db86a67b36bbf1d2ff96bd7626bbfb2408
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-11-24 14:00:07 +0300

    Add audio feedbacks when HRM connection lost or restored

---
+++ app/src/org/runnerup/workout/feedback/HRMStateChangeFeedback.java
@@ -0,0 +1,22 @@
+package org.runnerup.workout.feedback;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.runnerup.R;
+import org.runnerup.workout.HRMStateTrigger;
+import org.runnerup.workout.Scope;
+import org.runnerup.workout.Workout;
+
+public class HRMStateChangeFeedback extends AudioFeedback {
+    public HRMStateChangeFeedback(HRMStateTrigger trigger) {
+        super(0);
+    }
+
+    protected String getCue(Workout w, Context ctx) {
+        String cue =  ctx.getResources().getString((w.getHeartRate(Scope.CURRENT) == 0)
+                ? R.string.cur_hrm_connection_lost
+                : R.string.cue_hrm_connection_restored);
+        return (cue);
+    }
+}
