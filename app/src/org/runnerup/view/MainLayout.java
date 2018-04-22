471ce5ed1352619300fc9994e2f8fd01f75c0e01
Gerhard Olsson <gerhard.nospam@gmail.com>
2018-04-22 16:21:43 +0200

    code lint suppression after tools update

--- app/src/org/runnerup/view/MainLayout.java
+++ app/src/org/runnerup/view/MainLayout.java
@@ -78,7 +78,7 @@ public class MainLayout extends TabActivity
     }
 
     private View getTabView(CharSequence label, int iconResource) {
-        View tabView = getLayoutInflater().inflate(R.layout.bottom_tab_indicator, null);
+        @SuppressLint("InflateParams")View tabView = getLayoutInflater().inflate(R.layout.bottom_tab_indicator, null);
         ((TextView)tabView.findViewById(R.id.title)).setText(label);
         Drawable icon = ContextCompat.getDrawable(this, iconResource);
         ((ImageView)tabView.findViewById(R.id.icon)).setImageDrawable(icon);
