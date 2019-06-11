912b43ebf52c890a124d5dabf73d98aed752f594
Gerhard Olsson <gerhardol@users.noreply.github.com>
2019-06-11 23:22:50 +0200

    Remove Froyo product flavor
    Froyo variant fails to start on Gingerbread emulator
    Removing workarounds for Froyo incompatible libs
    
    Some Lint rearrangements for Latest

--- app/src/org/runnerup/notification/GpsBoundState.java
+++ app/src/org/runnerup/notification/GpsBoundState.java
@@ -4,11 +4,11 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import org.runnerup.R;
 import org.runnerup.common.util.Constants;
-import org.runnerup.util.SupportWrapper;
 import org.runnerup.view.MainLayout;
 
 
@@ -18,20 +18,22 @@ public class GpsBoundState implements NotificationState {
     public GpsBoundState(Context context) {
 
         String chanId = NotificationStateManager.getChannelId(context);
-        NotificationCompat.Builder builder = SupportWrapper.Builder(context, chanId);
         Intent i = new Intent(context, MainLayout.class);
         i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         i.putExtra(Constants.Intents.FROM_NOTIFICATION, true);
         PendingIntent pi = PendingIntent.getActivity(context, 0, i, 0);
 
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, chanId);
         builder.setContentIntent(pi);
         builder.setContentTitle(context.getString(R.string.Activity_ready));
         builder.setContentText(context.getString(R.string.Ready_to_start_running));
         builder.setSmallIcon(R.drawable.ic_stat_notify);
         builder.setOnlyAlertOnce(true);
-        org.runnerup.util.NotificationCompat.setLocalOnly(builder);
-        org.runnerup.util.NotificationCompat.setVisibility(builder);
-        org.runnerup.util.NotificationCompat.setCategory(builder);
+        builder.setLocalOnly(true);
+        if (Build.VERSION.SDK_INT >= 21) {
+            builder.setVisibility(Notification.VISIBILITY_PUBLIC);
+            builder.setCategory(Notification.CATEGORY_SERVICE);
+        }
 
         notification = builder.build();
     }
