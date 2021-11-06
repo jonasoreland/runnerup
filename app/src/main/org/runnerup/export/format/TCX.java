46810acec7d3a9a392334d5e2621bcf415be9da5
Gerhard Olsson <gerhardol@users.noreply.github.com>
2021-11-06 23:16:23 +0100

    fixup! Externalize KXmlSerializer.

--- app/src/main/org/runnerup/export/format/TCX.java
+++ app/src/main/org/runnerup/export/format/TCX.java
@@ -22,12 +22,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
 import android.util.Pair;
 
+import org.kxml2.io.KXmlSerializer;
 import org.runnerup.common.util.Constants.DB;
 import org.runnerup.db.PathCursor;
 import org.runnerup.db.PathSimplifier;
-//import org.runnerup.util.KXmlSerializer;
-import org.kxml2.io.KXmlSerializer;
-
 import org.runnerup.workout.Sport;
 
 import java.io.IOException;
