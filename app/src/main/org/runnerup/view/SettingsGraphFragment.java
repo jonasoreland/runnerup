0ed09b49b34c5a95ba308caf506a0f9292145b2c
Robert Jonsson <robert.jonsson@miun.se>
2024-03-06 19:12:50 +0100

    Migrate graph preferences to AndroidX Preference library
    Declaring nested hierarchies within the same XML resource using a nested PreferenceScreen (as the old settings.xml did) is no longer supported. Use nested Fragment objects instead.

---
+++ app/src/main/org/runnerup/view/SettingsGraphFragment.java
@@ -0,0 +1,15 @@
+package org.runnerup.view;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import org.runnerup.R;
+
+public class SettingsGraphFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_graph, rootKey);
+    }
+}
