d18ebb7df47c31e74dbeaaa5fc6ea8d71bbe9bee
Robert Jonsson <robert.jonsson@miun.se>
2024-03-06 18:55:28 +0100

    Migrate map preferences to AndroidX Preference library
    Declaring nested hierarchies within the same XML resource using a nested PreferenceScreen (as the old settings.xml did) is no longer supported. Use nested Fragment objects instead.

---
+++ app/src/main/org/runnerup/view/SettingsMapFragment.java
@@ -0,0 +1,15 @@
+package org.runnerup.view;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import org.runnerup.R;
+
+public class SettingsMapFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_map, rootKey);
+    }
+}
