9df1cd2fda6fdfabeb5e8471dedf23bc483f2f77
Gerhard Olsson <gerhardol@users.noreply.github.com>
2024-06-09 14:11:53 +0200

    Analyzer: Foreground service service type

--- app/src/main/org/runnerup/notification/ForegroundNotificationDisplayStrategy.java
+++ app/src/main/org/runnerup/notification/ForegroundNotificationDisplayStrategy.java
@@ -1,7 +1,14 @@
 package org.runnerup.notification;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.Service;
+import android.content.pm.PackageManager;
+import android.content.pm.ServiceInfo;
+import android.os.Build;
+
+import androidx.core.app.ServiceCompat;
+import androidx.core.content.ContextCompat;
 
 
 public class ForegroundNotificationDisplayStrategy implements NotificationDisplayStrategy {
@@ -13,11 +20,21 @@ public class ForegroundNotificationDisplayStrategy implements NotificationDispla
 
     @Override
     public void notify(int notificationId, Notification notification) {
-        service.startForeground(notificationId, notification);
+        int type = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (ContextCompat.checkSelfPermission(service, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                type = ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+                && ContextCompat.checkSelfPermission(service, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
+                    type |= ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH;
+            }
+        }
+        ServiceCompat.startForeground(service, notificationId, notification, type);
     }
 
     @Override
     public void cancel(int notificationId) {
-        service.stopForeground(true);
+        ServiceCompat.stopForeground(service, ServiceCompat.STOP_FOREGROUND_REMOVE);
     }
 }
