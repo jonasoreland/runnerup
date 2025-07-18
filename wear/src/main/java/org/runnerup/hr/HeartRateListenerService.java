6e070ae3d65d6fe597dc91bd71f7eb6ba42f33c3
Robert Jonsson <robert.jonsson@miun.se>
2025-07-18 20:24:50 +0200

    Implement service to receive HR control messages from phone
    Introduces HeartRateListenerService, extending
    WearableListenerService. This service acts as the entry point for
    commands sent from the phone to manage heart rate monitoring on
    the wearable (start and stop). The service is configured to
    filter messages only for the "/org.runnerup/hr" path.
    
    This initial version is mainly a skeleton, with all methods being
    placeholders (stubs) for now.

---
+++ wear/src/main/java/org/runnerup/hr/HeartRateListenerService.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2014 robert.jonsson75@gmail.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.runnerup.hr;
+
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * A {@link WearableListenerService} that listens for messages from a connected phone
+ * to control heart rate monitoring on the wearable device.
+ * <p>
+ * It receives messages via the Wearable Data Layer. Specifically, it handles:
+ * <ul>
+ *     <li>{@link Constants.Wear.Path#MSG_CMD_HR_START}: Starts the {@link HeartRateService},
+ *     passing the source node ID of the phone as an extra.</li>
+ *     <li>{@link Constants.Wear.Path#MSG_CMD_HR_STOP}: Stops the {@link HeartRateService}.</li>
+ * </ul>
+ */
+public class HeartRateListenerService extends WearableListenerService {
+    private static final String TAG = "HeartRateListenerService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull MessageEvent messageEvent) {
+        Log.d(TAG, "onMessageReceived: " + messageEvent.getPath() + " from " + messageEvent.getSourceNodeId());
+    }
+}
