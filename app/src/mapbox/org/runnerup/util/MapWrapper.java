2107b63544ce0e8305bd6aa538f7406f98b4cee0
Gerhard Olsson <gerhardol@users.noreply.github.com>
2026-01-01 12:36:05 +0100

    fix: use Executor for background tasks
    Mostly using Gemini, some manual fixes and updates to comments etc.

--- app/src/mapbox/org/runnerup/util/MapWrapper.java
+++ app/src/mapbox/org/runnerup/util/MapWrapper.java
@@ -19,13 +19,11 @@ package org.runnerup.util;
 
 import static org.runnerup.util.Formatter.Format.TXT_SHORT;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewTreeObserver;
@@ -49,8 +47,9 @@ import com.mapbox.mapboxsdk.utils.ColorUtils;
 import com.mapbox.pluginscalebar.ScaleBarOptions;
 import com.mapbox.pluginscalebar.ScaleBarPlugin;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.runnerup.BuildConfig;
 import org.runnerup.R;
 import org.runnerup.common.util.Constants;
@@ -67,8 +66,14 @@ public class MapWrapper implements Constants {
   private final Context context;
   private final Formatter formatter;
 
+  private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
   public MapWrapper(
-      Context context, SQLiteDatabase mDB, long mID, Formatter formatter, Object mapView) {
+      Context context,
+      SQLiteDatabase mDB,
+      long mID,
+      Formatter formatter,
+      java.lang.Object mapView) {
     this.context = context;
     this.mDB = mDB;
     this.mID = mID;
@@ -97,8 +102,8 @@ public class MapWrapper implements Constants {
               mapStyle -> {
                 lineManager = new LineManager(mapView, mapboxMap, mapStyle);
                 symbolManager = new SymbolManager(mapView, mapboxMap, mapStyle);
-                // Map is set up and the style has loaded
-                new LoadRoute().execute(new LoadParam(context, mDB, mID, mapboxMap));
+                // Map is set up and the style has loaded, now load the route data
+                loadRouteAsync(mapboxMap);
               });
         });
   }
@@ -128,6 +133,8 @@ public class MapWrapper implements Constants {
   }
 
   public void onDestroy() {
+    executor.shutdown();
+
     if (lineManager != null) {
       lineManager.onDestroy();
     }
@@ -137,213 +144,207 @@ public class MapWrapper implements Constants {
     mapView.onDestroy();
   }
 
-  class Route {
-    Route(Context context, MapboxMap map) {
-      this.context = context;
-      this.map = map;
-    }
-
-    final List<LatLng> path = new ArrayList<>(10);
-    final ArrayList<SymbolOptions> markers = new ArrayList<>(10);
-    final Context context;
-    final MapboxMap map;
+  /** Class to hold the processed route data from the background task. */
+  static class Route {
+    final java.util.List<LatLng> path = new ArrayList<>(10);
+    final java.util.ArrayList<SymbolOptions> markers = new ArrayList<>(10);
   }
 
-  private class LoadParam {
-    LoadParam(Context context, SQLiteDatabase mDB, long mID, MapboxMap map) {
-      this.context = context;
-      this.mDB = mDB;
-      this.mID = mID;
-      this.map = map;
-    }
+  /**
+   * Kicks off the process of loading route data on a background thread and then updating the UI.
+   *
+   * @param mapboxMap The map instance to update once data is loaded.
+   */
+  private void loadRouteAsync(final MapboxMap mapboxMap) {
+    executor.execute(
+        () -> {
+          // DB queries, data processing on the background thread
+          final Route route = loadRouteDataInBackground();
 
-    final Context context;
-    final SQLiteDatabase mDB;
-    final long mID;
-    final MapboxMap map;
+          // update the UI on the main thread
+          mapView.post(() -> onRouteLoaded(route, mapboxMap));
+        });
   }
 
-  @SuppressLint("StaticFieldLeak")
-  private class LoadRoute extends AsyncTask<LoadParam, Void, Route> {
-    @Override
-    protected Route doInBackground(LoadParam... params) {
+  /**
+   * This method runs on a background thread. It queries the database and processes the location
+   * data into a format suitable for the map, without touching any UI components.
+   *
+   * @return A {@link Route} object containing the processed path and markers.
+   */
+  private Route loadRouteDataInBackground() {
+    Route route = new Route();
+    LocationEntity.LocationList<LocationEntity> ll = new LocationEntity.LocationList<>(mDB, mID);
+    int lastLap = 0;
+    for (LocationEntity loc : ll) {
+      LatLng point = new LatLng(loc.getLatitude(), loc.getLongitude());
+      route.path.add(point);
 
-      Route route = new Route(params[0].context, params[0].map);
-      LocationEntity.LocationList<LocationEntity> ll =
-          new LocationEntity.LocationList<>(params[0].mDB, params[0].mID);
-      int lastLap = 0;
-      for (LocationEntity loc : ll) {
-        LatLng point = new LatLng(loc.getLatitude(), loc.getLongitude());
-        route.path.add(point);
+      java.lang.Integer type;
+      // Start/end markers are not set in db, special handling
+      if (route.markers.isEmpty()) {
+        type = DB.LOCATION.TYPE_START;
+      } else {
+        type = loc.getType();
+      }
 
-        Integer type;
-        // Start/end markers are not set in db, special handling
-        if (route.markers.isEmpty()) {
-          type = DB.LOCATION.TYPE_START;
+      java.lang.String iconImage;
+      if (type != DB.LOCATION.TYPE_START && lastLap != loc.getLap()) {
+        lastLap = loc.getLap();
+        iconImage = "lap";
+      } else if (type == DB.LOCATION.TYPE_START
+          || type == DB.LOCATION.TYPE_END
+          || type == DB.LOCATION.TYPE_PAUSE
+          || type == DB.LOCATION.TYPE_RESUME) {
+        iconImage = type.toString();
+      } else {
+        iconImage = null;
+      }
+
+      if (iconImage != null) {
+        // TBD Implement Info popup with the info instead, using the annotaion plugin (currently
+        // no examples)
+        java.lang.String info;
+        if (type == DB.LOCATION.TYPE_START) {
+          info = null;
         } else {
-          type = loc.getType();
+          info =
+              (iconImage.equals("lap") ? "#" + loc.getLap() + "\n" : "")
+                  + formatter.formatDistance(TXT_SHORT, loc.getDistance().longValue())
+                  + "\n"
+                  + formatter.formatElapsedTime(TXT_SHORT, Math.round(loc.getElapsed() / 1000.0));
         }
 
-        String iconImage;
-        if (type != DB.LOCATION.TYPE_START && lastLap != loc.getLap()) {
-          lastLap = loc.getLap();
-          iconImage = "lap";
-        } else if (type == DB.LOCATION.TYPE_START
-            || type == DB.LOCATION.TYPE_END
-            || type == DB.LOCATION.TYPE_PAUSE
-            || type == DB.LOCATION.TYPE_RESUME) {
-          iconImage = type.toString();
-        } else {
-          iconImage = null;
-        }
-
-        if (iconImage != null) {
-          // TBD Implement Info popup with the info instead, using the annotaion plugin (currently
-          // no examples)
-          String info;
-          if (type == DB.LOCATION.TYPE_START) {
-            info = null;
-          } else {
-            info =
-                (iconImage.equals("lap") ? "#" + loc.getLap() + "\n" : "")
-                    + formatter.formatDistance(TXT_SHORT, loc.getDistance().longValue())
-                    + "\n"
-                    + formatter.formatElapsedTime(TXT_SHORT, Math.round(loc.getElapsed() / 1000.0));
-          }
-
-          SymbolOptions m =
-              new SymbolOptions()
-                  .withLatLng(point)
-                  .withIconImage(iconImage)
-                  .withIconAnchor(Property.ICON_ANCHOR_BOTTOM)
-                  .withTextField(info)
-                  .withTextAnchor(Property.TEXT_ANCHOR_TOP);
-          route.markers.add(m);
-        }
-      }
-      ll.close();
-
-      // Track is normally ended with a pause, not always followed by an end
-      // Ignore the pause
-      if (route.markers.size() >= 2) {
-        SymbolOptions m = route.markers.get(route.markers.size() - 2);
-        SymbolOptions me = route.markers.get(route.markers.size() - 1);
-        if (m.getIconImage().equals(((Integer) DB.LOCATION.TYPE_PAUSE).toString())
-            && me.getIconImage().equals(((Integer) DB.LOCATION.TYPE_END).toString())) {
-          route.markers.remove(route.markers.size() - 2);
-        }
-      }
-
-      if (route.markers.size() >= 1) {
-        SymbolOptions me = route.markers.get(route.markers.size() - 1);
-        if (me.getIconImage().equals(((Integer) DB.LOCATION.TYPE_PAUSE).toString())) {
-          me.withIconImage(((Integer) DB.LOCATION.TYPE_END).toString());
-        }
-      }
-
-      return route;
-    }
-
-    @SuppressLint("ObsoleteSdkInt")
-    @Override
-    protected void onPostExecute(Route route) {
-
-      if (route != null && route.map != null) {
-
-        ScaleBarPlugin scaleBarPlugin = new ScaleBarPlugin(mapView, route.map);
-
-        scaleBarPlugin.create(new ScaleBarOptions(route.context));
-
-        if (route.path.size() > 1) {
-          LineOptions lineOptions =
-              new LineOptions()
-                  .withLatLngs(route.path)
-                  .withLineColor(ColorUtils.colorToRgbaString(Color.RED))
-                  .withLineWidth(3.0f);
-          lineManager.create(lineOptions);
-          Log.v(getClass().getName(), "Added line");
-
-          final LatLngBounds box = new LatLngBounds.Builder().includes(route.path).build();
-          final CameraUpdate initialCameraPosition = CameraUpdateFactory.newLatLngBounds(box, 50);
-          route.map.moveCamera(initialCameraPosition);
-
-          // Since MapBox 4.2.0-beta.3 moving the camera in onMapReady is not working if map is not
-          // visible
-          // The proper solution is a redesign using fragments, see
-          // https://github.com/mapbox/mapbox-gl-native/issues/6855#event-841575956
-          // A workaround is to try move the camera at view updates as long as position is 0,0
-          mapView
-              .getViewTreeObserver()
-              .addOnGlobalLayoutListener(
-                  new ViewTreeObserver.OnGlobalLayoutListener() {
-                    // We check which build version we are using.
-                    @Override
-                    public void onGlobalLayout() {
-                      if (route.map.getCameraPosition().target.getLatitude() != 0
-                          || route.map.getCameraPosition().target.getLongitude() != 0) {
-                        mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                      } else {
-                        route.map.moveCamera(initialCameraPosition);
-                      }
-                    }
-                  });
-        }
-
-        // Images id from text
-        route
-            .map
-            .getStyle()
-            .addImage(
-                ((Integer) DB.LOCATION.TYPE_START).toString(),
-                Objects.requireNonNull(
-                    BitmapUtils.getBitmapFromDrawable(
-                        AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_start))),
-                false);
-        route
-            .map
-            .getStyle()
-            .addImage(
-                ((Integer) DB.LOCATION.TYPE_END).toString(),
-                Objects.requireNonNull(
-                    BitmapUtils.getBitmapFromDrawable(
-                        AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_end))),
-                false);
-        route
-            .map
-            .getStyle()
-            .addImage(
-                ((Integer) DB.LOCATION.TYPE_PAUSE).toString(),
-                Objects.requireNonNull(
-                    BitmapUtils.getBitmapFromDrawable(
-                        AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_pause))),
-                false);
-        route
-            .map
-            .getStyle()
-            .addImage(
-                ((Integer) DB.LOCATION.TYPE_RESUME).toString(),
-                Objects.requireNonNull(
-                    BitmapUtils.getBitmapFromDrawable(
-                        AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_resume))),
-                false);
-        route
-            .map
-            .getStyle()
-            .addImage(
-                "lap",
-                Objects.requireNonNull(
-                    BitmapUtils.getBitmapFromDrawable(
-                        AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_lap))),
-                false);
-
-        symbolManager.setIconAllowOverlap(true);
-        if (route.markers.size() > 0) {
-          for (SymbolOptions m : route.markers) {
-            symbolManager.create(m);
-          }
-        }
-        Log.v(getClass().getName(), "Added " + route.markers.size() + " markers");
+        SymbolOptions m =
+            new SymbolOptions()
+                .withLatLng(point)
+                .withIconImage(iconImage)
+                .withIconAnchor(Property.ICON_ANCHOR_BOTTOM)
+                .withTextField(info)
+                .withTextAnchor(Property.TEXT_ANCHOR_TOP);
+        route.markers.add(m);
       }
     }
+    ll.close();
+
+    // Track is normally ended with a pause, not always followed by an end
+    // Ignore the pause
+    if (route.markers.size() >= 2) {
+      SymbolOptions m = route.markers.get(route.markers.size() - 2);
+      SymbolOptions me = route.markers.get(route.markers.size() - 1);
+      if (m.getIconImage().equals(((Integer) DB.LOCATION.TYPE_PAUSE).toString())
+          && me.getIconImage().equals(((Integer) DB.LOCATION.TYPE_END).toString())) {
+        route.markers.remove(route.markers.size() - 2);
+      }
+    }
+
+    if (!route.markers.isEmpty()) {
+      SymbolOptions me = route.markers.get(route.markers.size() - 1);
+      if (me.getIconImage().equals(((Integer) DB.LOCATION.TYPE_PAUSE).toString())) {
+        me.withIconImage(((Integer) DB.LOCATION.TYPE_END).toString());
+      }
+    }
+
+    return route;
+  }
+
+  /**
+   * This method runs on the main UI thread. It takes the processed route data and applies it to the
+   * map, drawing the polyline and markers.
+   *
+   * @param route The processed route data.
+   * @param mapboxMap The map instance to update.
+   */
+  private void onRouteLoaded(Route route, MapboxMap mapboxMap) {
+    if (route == null) {
+      return;
+    }
+
+    ScaleBarPlugin scaleBarPlugin = new ScaleBarPlugin(mapView, mapboxMap);
+    scaleBarPlugin.create(new ScaleBarOptions(context));
+
+    if (route.path.size() > 1) {
+      LineOptions lineOptions =
+          new LineOptions()
+              .withLatLngs(route.path)
+              .withLineColor(ColorUtils.colorToRgbaString(Color.RED))
+              .withLineWidth(3.0f);
+      lineManager.create(lineOptions);
+      Log.v(getClass().getName(), "Added line");
+
+      final LatLngBounds box = new LatLngBounds.Builder().includes(route.path).build();
+      final CameraUpdate initialCameraPosition = CameraUpdateFactory.newLatLngBounds(box, 50);
+      mapboxMap.moveCamera(initialCameraPosition);
+
+      // Since MapBox 4.2.0-beta.3 moving the camera in onMapReady is not working if map is not
+      // visible
+      // The proper solution is a redesign using fragments, see
+      // https://github.com/mapbox/mapbox-gl-native/issues/6855#event-841575956
+      // A workaround is to try move the camera at view updates as long as position is 0,0
+      mapView
+          .getViewTreeObserver()
+          .addOnGlobalLayoutListener(
+              new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                  if (mapboxMap.getCameraPosition().target.getLatitude() != 0
+                      || mapboxMap.getCameraPosition().target.getLongitude() != 0) {
+                    mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                  } else {
+                    mapboxMap.moveCamera(initialCameraPosition);
+                  }
+                }
+              });
+    }
+
+    // Images id from text
+    Objects.requireNonNull(mapboxMap
+                    .getStyle())
+        .addImage(
+            ((Integer) DB.LOCATION.TYPE_START).toString(),
+            Objects.requireNonNull(
+                BitmapUtils.getBitmapFromDrawable(
+                    AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_start))),
+            false);
+    mapboxMap
+        .getStyle()
+        .addImage(
+            ((Integer) DB.LOCATION.TYPE_END).toString(),
+            Objects.requireNonNull(
+                BitmapUtils.getBitmapFromDrawable(
+                    AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_end))),
+            false);
+    mapboxMap
+        .getStyle()
+        .addImage(
+            ((Integer) DB.LOCATION.TYPE_PAUSE).toString(),
+            Objects.requireNonNull(
+                BitmapUtils.getBitmapFromDrawable(
+                    AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_pause))),
+            false);
+    mapboxMap
+        .getStyle()
+        .addImage(
+            ((Integer) DB.LOCATION.TYPE_RESUME).toString(),
+            Objects.requireNonNull(
+                BitmapUtils.getBitmapFromDrawable(
+                    AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_resume))),
+            false);
+    mapboxMap
+        .getStyle()
+        .addImage(
+            "lap",
+            Objects.requireNonNull(
+                BitmapUtils.getBitmapFromDrawable(
+                    AppCompatResources.getDrawable(context, R.drawable.ic_map_marker_lap))),
+            false);
+
+    symbolManager.setIconAllowOverlap(true);
+    if (!route.markers.isEmpty()) {
+      for (SymbolOptions m : route.markers) {
+        symbolManager.create(m);
+      }
+    }
+    Log.v(getClass().getName(), "Added " + route.markers.size() + " markers");
   }
 }
