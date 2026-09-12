771124411ce2040d2d3cd9885934909942b1a543
Robert Jonsson <robert.jonsson@miun.se>
2026-09-12 20:19:55 +0200

    Update copy preference only on successful file copy

--- app/src/main/org/runnerup/view/MainLayout.java
+++ app/src/main/org/runnerup/view/MainLayout.java
@@ -348,14 +348,13 @@ public class MainLayout extends AppCompatActivity {
             continue;
           }
 
-          pref.edit().putBoolean(key, true).apply();
-
           Log.d(getClass().getName(), "Copying: " + dst);
           InputStream input = null;
           try {
             input = mgr.open(src);
             FileUtil.copy(input, dst);
             handleHooks(add);
+            pref.edit().putBoolean(key, true).apply(); // Store successfull copy
           } catch (IOException e) {
             e.printStackTrace();
           } finally {
