aeb8a388636839d827df6b6ee9a59ec814e10696
Gerhard Olsson <gerhardol@users.noreply.github.com>
2026-09-13 14:33:51 +0200

    fix: ble hr deprecation fixes
    Update code with api-13 to remove deprecated calls.
    (suppress for <api-33).
    
    Support HR transmitter that do not support battery or firmware.
    This occurs for instance for Garmin watches set to send HR
    over bluetooth.

--- hrdevice/src/org/runnerup/hr/BtHRBase.java
+++ hrdevice/src/org/runnerup/hr/BtHRBase.java
@@ -22,14 +22,17 @@ import android.util.Log;
 import java.util.UUID;
 
 abstract class BtHRBase implements HRProvider {
-  static final UUID HRP_SERVICE = UUID.fromString("0000180D-0000-1000-8000-00805f9b34fb");
-  static final UUID BATTERY_SERVICE = UUID.fromString("0000180f-0000-1000-8000-00805f9b34fb");
+  static final UUID HEART_RATE_SERVICE_UUID =
+      UUID.fromString("0000180D-0000-1000-8000-00805f9b34fb");
+  static final UUID BATTERY_SERVICE_UUID = UUID.fromString("0000180f-0000-1000-8000-00805f9b34fb");
   static final UUID FIRMWARE_REVISON_UUID = UUID.fromString("00002a26-0000-1000-8000-00805f9b34fb");
-  static final UUID DIS_UUID = UUID.fromString("0000180a-0000-1000-8000-00805f9b34fb");
+  static final UUID DEVICE_INFORMATION_SERVICE_UUID =
+      UUID.fromString("0000180a-0000-1000-8000-00805f9b34fb");
   static final UUID HEART_RATE_MEASUREMENT_CHARAC =
       UUID.fromString("00002A37-0000-1000-8000-00805f9b34fb");
   static final UUID BATTERY_LEVEL_CHARAC = UUID.fromString("00002A19-0000-1000-8000-00805f9b34fb");
-  static final UUID CCC = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+  static final UUID CLIENT_CHARACTERISTIC_CONFIGURATION =
+      UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
   static final UUID HARDWARE_REVISON_UUID = UUID.fromString("00002a27-0000-1000-8000-00805f9b34fb");
 
   HRProvider.HRClient hrClient;
